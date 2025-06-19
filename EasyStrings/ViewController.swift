fcccb036bcc52fe8d4086638832e969863732fe5
coffegorio <coffegorio@vk.com>
2025-06-19 23:01:31 +0300

    Initial Commit

---
+++ EasyStrings/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyStrings
+//
+//  Created by Егорио on 19.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
